081424762f649633c14ef98078331f13f33ed5fb
Gordon Nongkynrih <gordon@dolphinze.com>
2025-06-26 10:35:26 +0530

    build

---
+++ README.md
@@ -0,0 +1,41 @@
+nodejs --> https://nodejs.org/en/download/
+composer ---> https://getcomposer.org
+php
+mysql
+vscode
+
+Laravel - -PHP Framework
+--> Livewire
+CSS --> tailwindCSS is a CSS framework
+JS --> AlpineJS
+
+nielit2.1 5G
+password: nielit364#
+
+TO create a new project
+composer create-project larave/laravel projectName
+After creating the project we run the command
+npm install --> install all the packages/modules
+npm run dev
+
+TO run a server
+php artisan serve
+
+MVC --> Model View Controller
+View --> is what we see
+view is inside resources folder
+
+model --> table
+model is inside app/Models folder
+
+controller --> interaction between model and view
+controller is inside app/Http/Controllers folder
+
+install livewire
+composer require livewire/livewire
+
+intall tailwindcss
+npm install tailwindcss @tailwindcss/vite
+npm run dev
+
+https://tailwindcss.com/docs
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+     @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-purple-800 text-white">
+    <h1 class="text-center text-3xl mb-10 mt-10">ST Marys</h1>
+    <h5 class="text-center text-xl"> Welcome to Laravel</h5>
+    <div class="flex  mt-24 justify-evenly">
+        <button class="bg-blue-500 text-white p-5 rounded-4xl shadow-xl border-2 border-pink-500 hover:bg-pink-500">CLick here</button>
+        <button class="bg-blue-500 text-white p-5 rounded-4xl shadow-xl border-2 border-pink-500 hover:bg-pink-500">CLose</button>
+        <button class="bg-blue-500 text-white p-5 rounded-4xl shadow-xl border-2 border-pink-500 hover:bg-pink-500">CLose</button>
+    </div>
+    
+</body>
+</html>
